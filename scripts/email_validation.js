a4b4890ee7ddd4213e8519d16de9690787aad926
Szymon Paprocki <s.paprocki.vs@gmail.com>
2022-11-03 11:54:00 +0100

    first commit

---
+++ scripts/email_validation.js
@@ -0,0 +1,45 @@
+const emailInputElement = document.getElementById('email-address');
+const submitButtonElement = document.getElementById('submit-button');
+const errorMessageElement = document.getElementById('error-message');
+
+submitButtonElement.addEventListener('click', evaluateEmail);
+
+function evaluateEmail(event) {
+  event.preventDefault();
+  if (!emailIsValid(emailInputElement)) {
+    showError();
+    moveSubmitButtonDown();
+  } else {
+    clearError();
+    moveSubmitButtonUp();
+  }
+}
+
+function emailIsValid(emailInput) {
+  const emailInputText = emailInput.value;
+  const emailRegex = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+
+  if (emailInputText.match(emailRegex)) {
+    return true;
+  } else return false;
+}
+
+function showError() {
+  errorMessageElement.classList.add('visible');
+  emailInputElement.classList.add('error');
+}
+
+function clearError() {
+  errorMessageElement.classList.remove('visible');
+  emailInputElement.classList.remove('error');
+}
+
+function moveSubmitButtonDown() {
+  submitButtonElement.style.top =
+    'calc(2rem + clamp(3.125rem, 1.5vw + 2.77rem, 4.125rem))';
+}
+
+function moveSubmitButtonUp() {
+  submitButtonElement.style.top =
+    'calc(clamp(3.125rem, 1.5vw + 2.77rem, 4.125rem))';
+}
